5bea3188d856ef7658f383403f9930b6d8b07cb9
Nate Sales <nate@natesales.net>
2022-04-08 18:38:00 -0700

    feat: add field

---
+++ src/components/widgets/Field.svelte
@@ -0,0 +1,28 @@
+<script>
+    import {onMount} from "svelte";
+
+    let canvas;
+    onMount(() => {
+        let ctx = canvas.getContext("2d");
+    })
+</script>
+
+<main>
+    <canvas bind:this={canvas}></canvas>
+</main>
+
+<style>
+    canvas {
+        background: url("/field/2022.png");
+        background-size: 100% 100%;
+        height: 500px;
+    }
+
+    main {
+        padding: 10px;
+        margin-right: 10px;
+        margin-bottom: 10px;
+        border: white solid 1px;
+        border-radius: 5px;
+    }
+</style>
