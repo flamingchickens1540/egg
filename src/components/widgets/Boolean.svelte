0f868d4127e5b0973e58e5cc4f72191ec866e112
Nate Sales <nate@natesales.net>
2022-04-08 18:37:50 -0700

    feat: add boolean

--- src/components/widgets/Boolean.svelte
+++ src/components/widgets/Boolean.svelte
@@ -9,5 +9,18 @@
 
 <Wrapper>
     <span>{label}</span>
+    <br>
     <input bind:value on:change={() => {NetworkTables.putValue(key, value)}} type="checkbox">
 </Wrapper>
+
+<style>
+    br {
+        padding-top: 20px;
+        margin-bottom: 10px;
+    }
+
+    input {
+        height: 20px !important;
+        width: 20px !important;
+    }
+</style>
